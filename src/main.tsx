5a209e1dad2fcc2b6619517e8d26c849b501bb26
LunaGomez24 <neontime09@gmail.com>
2026-05-07 04:14:33 -0500

    feat: ajustar deploy con hash router

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,17 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { Authenticator } from './features/auth/Authenticator';
 import './index.css';
 import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Authenticator>
         <App />
       </Authenticator>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 );
 
